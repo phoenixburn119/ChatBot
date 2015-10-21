6bc8750c7f5002785fd99ae76977958d3583e768
Adam Kinzer <adamkinzer915@gmail.com>
2015-10-21 13:51:08 -0600

    Completed lengthChecker method

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Base version of the 2015 Chatbot class. Only stub methods are provided. Students will complete methods as part
  * of the project.
  * @author Adam Kinzer
- * @version 1.0 10/14/15
+ * @version 1.1 10/14/15 Completed the lengthChecker method.
  */
 public class Chatbot
 {
@@ -42,7 +42,15 @@ public class Chatbot
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
-		return false;
+		boolean hasLength = false;
+		if(currentInput != null)
+		{
+			if(currentInput.length() > 0)
+			{
+				hasLength = true;
+			}
+		}
+		return hasLength;
 	}
 	
 	/**
