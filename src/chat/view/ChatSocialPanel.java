8522a90169c33217da08a87ed553a609342349ff
Adam Kinzer <adamkinzer915@gmail.com>
2016-02-23 12:56:28 -0700

    Added near empty ChatSocialPanel

---
+++ src/chat/view/ChatSocialPanel.java
@@ -0,0 +1,44 @@
+package chat.view;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import chat.controller.ChatController;
+
+public class ChatSocialPanel extends JPanel
+{
+	private ChatController baseController;
+	private SpringLayout baseLayout;
+	private JLabel titleBar;
+	private JButton faceBookButton;
+	private JButton twitterButton;
+	
+	public ChatSocialPanel(ChatController baseController)
+	{
+		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		titleBar = new JLabel("ChatBot's Social Pages");
+		faceBookButton = new JButton("Facebook");
+		twitterButton = new JButton("Twitter");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
