3db6004fd0c54339415a5fe5db9adca5b0f92bc0
Adam Kinzer <adamkinzer915@gmail.com>
2016-03-07 12:41:42 -0700

    Added twitter object

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,17 @@
+package chat.model;
+
+import java.util.*;
+import twitter4j.*;
+
+public class CTECTwitter
+{
+	private ArrayList <Status> statusList;
+	private ArrayList <String> wordsList;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordsList = new ArrayList<String>();
+	}
+	
+}
