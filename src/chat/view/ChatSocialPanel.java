d7f1d04a0d4425ef0c1c43a5da7cf96551a33838
Adam Kinzer <adamkinzer915@gmail.com>
2016-03-09 13:38:42 -0700

    Added to socialPanel

--- src/chat/view/ChatSocialPanel.java
+++ src/chat/view/ChatSocialPanel.java
@@ -1,9 +1,15 @@
 package chat.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
+
 import chat.controller.ChatController;
 import chat.view.ChatPanel;
 
@@ -43,6 +49,12 @@ public class ChatSocialPanel extends JPanel
 	
 	private void setupListeners()
 	{
-		
+		twitterButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				//Assign to do something
+			}
+		});
 	}
 }
